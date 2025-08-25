034ae247ead46bba208694b233ae2d5d5c14b89b
Bidhisha Sardar <bidhishasardar07@gmail.com>
2025-08-25 14:38:53 +0530

    area of the circle.c

--- Day 2/area of the circle.c
+++ Day 2/area of the circle.c
@@ -4,7 +4,8 @@ int main()
 	int r,area;
 	printf("Enter the radius of the circle: ");
 	scanf("%d",&r);
-	area = (2*3.14*r*r);
+	area = (3.14*r*r);
 	printf("The area of the circle: %d",area);
 	return 0;
 }
+
